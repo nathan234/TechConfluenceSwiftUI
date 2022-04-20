4335b86b21b8ade0d38ce3efde9edde0aa263d59
Nathan Retta <nretta@gmail.com>
2022-04-20 09:56:40 -0600

    Initial Commit

---
+++ Shared/TechConfluenceSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechConfluenceSwiftUIApp.swift
+//  Shared
+//
+//  Created by Nathan Retta on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct TechConfluenceSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
