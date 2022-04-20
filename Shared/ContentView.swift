0953bb0ba78d9c43711789ffea645fe5e2dc1496
Nathan Retta <nretta@gmail.com>
2022-04-20 10:02:31 -0600

    title

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    @State var taps = 0
+        var body: some View {
+        NavigationView {
+            ZStack {
+                VStack {
+                    Text("You have tapped the button this many times:")
+                        .navigationBarTitle("TechConfluence")
+                    Text("\(taps)")
+                        .font(.system(size: 30))
+                        .fontWeight(.bold)
+                }
+                ZStack(alignment: .bottomTrailing) {
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    Button(action: {
+                        self.taps += 1
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .frame(width: 75, height: 75)
+                            .foregroundColor(Color(red: 153/255, green: 102/255, blue: 255/255))
+                            .shadow(color: .gray, radius: 0.2, x: 1, y: 1)
+                    }
+                }.padding()
+            }
+        }
+        .ignoresSafeArea()
     }
 }
